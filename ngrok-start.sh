99285996bc8f26bee082971a3b54fbf9be0291e3
kevin <da_kurlzzzzz@mail.ru>
2022-05-20 17:57:25 +0300

    removed redundant 'while'

--- ngrok-start.sh
+++ ngrok-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ngrok tcp --log stdout 22 | tee >(
-while ! grep "started tunnel" -m 1
-do
-    :
-done | sed -E 's#.*url=tcp://(.*)#://kevin@\1#' | xargs $(dirname $(realpath $0))/ngrok-bot.sh)
+    grep "started tunnel" -m 1 |
+    sed -E 's#.*url=tcp://(.*)#://kevin@\1#' |
+    xargs $(dirname $(realpath $0))/ngrok-bot.sh
+)
